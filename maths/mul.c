670a2d8a846fe2b8f1d3beae6821ebe231113d36
vitalspace <myglobalsounds@gmail.com>
2023-10-25 00:19:02 -0600

    add maths examples

---
+++ maths/mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int mult(int a, int b) {
+    return a * b;
+}
+
+int main () {
+    int a = 2;
+    int b = 4;
+    int result = mult(a, b);
+
+    printf("%d * %d = %d\n", a, b, result);
+}
---
+++ maths/sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int sum (int a, int b) {
+    return a + b;
+}
+
+int main () {
+    int a = 2;
+    int b = 3;
+    int result = sum(a, b);
+    printf("%d + %d = %d\n" , a, b, result );
+}
