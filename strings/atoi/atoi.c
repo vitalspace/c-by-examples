5ef424ef35c70df97636e5b268a0c08e2f02d1b9
vitalspace <myglobalsounds@gmail.com>
2023-10-25 19:06:22 -0600

    added comments

--- strings/atoi/atoi.c
+++ strings/atoi/atoi.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-    char str[] = "12345";
-    int number = atoi(str);
+    char str[] = "12345";  // A string representing an integer.
+    int number = atoi(str);  // Convert the string to an integer using atoi.
 
+    // Print the converted integer.
     printf("The integer is: %d\n", number);
-    return 0;
+
+    return 0;  // Return 0 to indicate successful program execution.
 }
