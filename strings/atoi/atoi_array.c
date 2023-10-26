9b5f25a864095c5c5fca1caa4c5643e6dd047ecf
vitalspace <myglobalsounds@gmail.com>
2023-10-25 19:07:21 -0600

    added comments

--- strings/atoi/atoi_array.c
+++ strings/atoi/atoi_array.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function to convert an array of string representations to integers and print them.
 void convertToInteger(char *arr[], int len) {
-    for(int i = 0; i < len; i++) {
-        int number = atoi(arr[i]);
-        printf("%d\n", number);
+    for (int i = 0; i < len; i++) {
+        int number = atoi(arr[i]);  // Convert the string to an integer using atoi.
+        printf("%d\n", number);     // Print the converted integer.
     }
 }
 
 int main() {
-    char *numbers[] = { "123124", "93845", "034950"};
-    int len = sizeof(numbers) / sizeof(numbers[0]);
+    // Array of string representations of integers.
+    char *numbers[] = {"123124", "93845", "034950"};
+    int len = sizeof(numbers) / sizeof(numbers[0]);  // Calculate the number of elements in the array.
+
+    // Call the function to convert and print the integer values.
     convertToInteger(numbers, len);
-    return 0;
+
+    return 0;  // Return 0 to indicate successful program execution.
 }
